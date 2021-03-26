e3bd5fe8b4cbfc20d6d21636a9845441761e0180
Francisco Lizcano <fjlizcano@misena.edu.co>
2021-03-25 19:58:28 -0500

    Carga inicial

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const nombre = req.query.nombre || 'desconocido';
+  res.send(`<h1>Hola ${nombre}!`);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
